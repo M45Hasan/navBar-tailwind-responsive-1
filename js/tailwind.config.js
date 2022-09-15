e0d5d0ed997c7c75397f801620bdb98b14515f04
M45Hasan <mmhasan045@gmail.com>
2022-09-15 13:23:28 +0600

    navBar

---
+++ js/tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        white: "rgba(238, 237, 222, 1)",
+        red: "#F40404",
+        yellow: "rgb(250 204 21)",
+      },
+      maxWidth: {
+        conatiner: "1000px",
+      },
+      fontFamily: {
+        pop: "Poppins",
+      },
+      backgroundImage: {
+        ban: "url('hom1.png')",
+
+      },
+    },
+  },
+};
